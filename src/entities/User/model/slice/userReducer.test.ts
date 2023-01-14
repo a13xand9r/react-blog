8fef528fe803dd53bc22149002c60c4a52fa825c
Александр Новиков <aleksandrnovikov@MacBook-Air-Aleksandr.local>
2023-01-14 14:05:49 +0500

    add test for user reducer

---
+++ src/entities/User/model/slice/userReducer.test.ts
@@ -0,0 +1,13 @@
+import { UserSchema } from 'entities/User';
+import { DeepPartial } from '@reduxjs/toolkit';
+import { userActions, userReducer } from './userSlice';
+
+describe('user reducer', () => {
+    it('auth data should be set', () => {
+        const userData = { id: '1', username: 'name1' };
+        const state: DeepPartial<UserSchema> = {};
+        const resultState = userReducer(state as UserSchema, userActions.setAuthData(userData));
+
+        expect(resultState).toEqual({ authUser: userData });
+    });
+});
