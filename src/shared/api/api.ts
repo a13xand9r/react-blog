c5fbd002106abeb576d4ecb52bc1d8c7f561d8d4
Александр Новиков <aleksandrnovikov@192.168.1.7>
2023-05-07 16:06:41 +0300

    add login interceptor for axios

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,7 +3,11 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/consts/localstorage';
 
 export const axiosInstance = axios.create({
     baseURL: __API_BASE_URL__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) ?? '',
-    },
+});
+
+axiosInstance.interceptors.request.use(config => {
+    if (config.headers) {
+        config.headers.authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) ?? '';
+    }
+    return config;
 });
