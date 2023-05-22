623e224d7c457046ae9a4936ad41c59594aee196
Aleksandr <a13x.student@yandex.ru>
2023-05-22 22:55:12 +0300

    add title outTagProp

--- src/shared/ui/Title/Title.tsx
+++ src/shared/ui/Title/Title.tsx
@@ -8,11 +8,13 @@ export const enum TitleTheme {
 }
 
 type TitleSize = 's' | 'm' | 'l' | 'xl';
+type OutTagType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
 interface TitleProps {
     className?: string;
     theme?: TitleTheme;
     size?: TitleSize;
+    OutTag?: OutTagType;
     center?: boolean;
 }
 
@@ -21,15 +23,16 @@ export const Title: FC<TitleProps> = ({
     children,
     center,
     size = 'm',
+    OutTag = 'h2',
     theme = TitleTheme.DEFAULT,
 }) => {
     return (
-        <h2
+        <OutTag
             className={classNames(className, styles.Title, styles[theme], styles[size], {
                 [styles.center]: center,
             })}
         >
             {children}
-        </h2>
+        </OutTag>
     );
 };
