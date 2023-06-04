02586b6ff12986289d0af67ea7a98ac6be5dde9c
Aleksandr <a13x.student@yandex.ru>
2023-06-04 18:38:08 +0300

    import fix in index file

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,8 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { StoreProvider } from 'app/providers/StoreProvider';
-
-import { App } from './app/App';
-import { ThemeProvider } from './shared/theme/ThemeContext';
+import { App } from 'app/App';
+import { ThemeProvider } from 'shared/theme/ThemeContext';
 
 import 'shared/config/i18n/i18n';
 import './app/styles/index.scss';
